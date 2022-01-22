8467f77031ee05a0a3aa8be8aa848067784d19fb
hekaahve <heini.k.ahven@student.jyu.fi>
2022-01-22 22:54:38 +0200

    chart fixations

--- baby-UI/src/app/info/info.component.ts
+++ baby-UI/src/app/info/info.component.ts
@@ -20,51 +20,40 @@ export class InfoComponent implements OnInit {
     title: {
       text: 'Age/weight'
     },
-    xAxis: {
-      type: 'category',
-      data: ['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        name: '2SD',
-        type: 'line',    
-        data: [4.5, 5.7, 7, 8.8, 9.6, 10.1, 10.5],
-        smooth: true
-      },
-      {
-        name: '1SD',
-        type: 'line',
-        data: [4.5, 5.7, 7, 8.8, 9.6, 10.1, 10.5],
-        smooth: true
-      },
-      {
-        name: '0SD',
-        type: 'line',
-        data: [4.5, 5.7, 7, 8.8, 9.6, 10.1, 10.5],
-        smooth: true
-      },
-      {
-        name: '-1SD',
-        type: 'line',
-        data: [4.5, 5.7, 7, 8.8, 9.6, 10.1, 10.5],
-        smooth: true
-      },
-      {
-        name: '-2SD',
-        type: 'line',
-        data: [4.5, 5.7, 7, 8.8, 9.6, 10.1, 10.5],
-        smooth: true
-      },
-      {
-        name: 'Ninni',
-        type: 'line',
-        data: [3.78, 3.526, 3.950, 4.51, 4.76, 5.285],
-        smooth: true
-      }
+    legend: {},
+  tooltip: {},
+  dataset: {
+    // Provide a set of data.
+    //dimensions: ['Age', '0.1', '0.2', '0.3'],
+    source: [
+      {"Age":"0", "Ninni":3.789, "+2SD":4.5, "+1SD":4},
+      {"Age":"0.1", "Ninni":3.950},
+      {"Age":"0.2", "Ninni":4.76},
+      {"Age":"0.3", "Ninni":5.285},
+
+      {"Age":"0", "+2SD":4.5},
+      {"Age":"0.1", "+2SD":5.6},
+      {"Age":"0.2", "+2SD":7},
+      {"Age":"0.3", "+2SD":8},
+      {"Age":"0.4", "+2SD":8.9},
+      {"Age":"0.5", "+2SD":9.5},
+
+      {"Age":"0", "+1SD":4},
+      {"Age":"0.1", "+1SD":5.1},
+      {"Age":"0.2", "+1SD":5.5},
+      {"Age":"0.3", "+1SD":7.2},
+      {"Age":"0.4", "+1SD":8},
+      {"Age":"0.5", "+1SD":8.6},
     ]
-  }
+  },
+  // Declare an x-axis (category axis).
+  // The category map the first row in the dataset by default.
+  xAxis: { type: 'category', name: "Age (months)" },
+  // Declare a y-axis (value axis).
+  yAxis: {name: "Weight (kg)"},
+  // Declare several 'bar' series,
+  // every series will auto-map to each rows by default. TODO: Continue ECharts Docs/Datasets
+  series: [{ type: 'line', color:'pink' }, { type: 'line' }, { type: 'line' }]
+  };
 
 }
