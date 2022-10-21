a1a28081b52f8cbdfc652f45f564b9f88fd25974
Heini Ahven <heini.ahven@gmail.com>
2022-10-21 20:55:21 +0300

    bug fixed

--- baby-UI/src/app/navbar/navbar.component.ts
+++ baby-UI/src/app/navbar/navbar.component.ts
@@ -3,13 +3,80 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  ChartOptions: any;
+  countryCasesChartOptions: any;
 
-  constructor() { }
-//TODO add some functionality
+  constructor() {}
+  //TODO add some functionality
   ngOnInit(): void {
+    this.setOptions();
   }
-
+  setOptions() {
+    this.ChartOptions = {
+      title: {
+        text: 'Age/weight',
+      },
+      legend: {},
+      tooltip: {},
+      dataset: [
+        {
+          dimensions: [
+            'age',
+            'weight',
+            'name',
+            { name: 'name', type: 'ordinal' }, // TODO: type to date
+          ],
+          source: [
+            { age: 1, weight: 5.5, name: 'ninni' },
+            { age: 2, weight: 6, name: 'ninni' },
+            { age: 3, weight: 6.6, name: 'ninni' },
+            { age: 3.4, weight: 5.5, name: 'ninni' },
+          ],
+        },
+        {
+          dimensions: [
+            'age',
+            'weight',
+            'name',
+            { name: 'name', type: 'ordinal' }, // TODO: type to date
+          ],
+          source: [
+            { age: 1, weight: 7, name: 'SD1' },
+            { age: 2, weight: 8, name: 'SD1' },
+            { age: 3, weight: 7.6, name: 'SD1' },
+          ],
+        },
+      ],
+      // The category map the first row in the dataset by default.
+      xAxis: { type: 'category', name: 'Age (months)' },
+      yAxis: { name: 'Weight (kg)' },
+      series: [
+        {
+          type: 'line',
+          datasetIndex: 0,
+          label: {
+            show: true,
+          },
+          encode: {
+            x: 'age',
+            y: 'weight',
+          },
+        },
+        {
+          type: 'line',
+          datasetIndex: 1,
+          label: {
+            show: true,
+          },
+          encode: {
+            x: 'age',
+            y: 'weight',
+          },
+        },
+      ],
+    };
+  }
 }
--- baby-UI/src/services/weights.service.ts
+++ baby-UI/src/services/weights.service.ts
@@ -1,32 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs/Observable'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 export interface Chart {
-  age: string,
-  weight: Number,
-  name: string
+  age: string; //change to number
+  weight: Number;
+  name: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeightsService {
   public Ninni: any;
   public Pos2SD: any[] = [];
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   getAll(): Observable<Chart[]> {
-  //https://blooming-castle-98347.herokuapp.com/api/charts/ninni
-  return this.http.get<Chart[]>('http://localhost:3001/api/charts', {observe: 'body', responseType: 'json'})
+    //https://blooming-castle-98347.herokuapp.com/api/charts/ninni
+    return this.http.get<Chart[]>('http://localhost:3001/api/charts', {
+      observe: 'body',
+      responseType: 'json',
+    });
   }
 
-  //TODO: Fix error from backend Cannot set headers after they are sent to the client
-  addValue(age: any, weight: any, name: any): Observable<any>{
-    var newChart = JSON.stringify({age: age, weight: weight, name: name})
-    const headers = { 'content-type': 'application/json'}
-    return this.http.post('http://localhost:3001/api/charts', newChart, {'headers':headers})
+  addValue(age: any, weight: any, name: any): Observable<any> {
+    var newChart = JSON.stringify({ age: age, weight: weight, name: name });
+    const headers = { 'content-type': 'application/json' };
+    return this.http.post('http://localhost:3001/api/charts', newChart, {
+      headers: headers,
+    });
   }
 }
